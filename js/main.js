3f77bc8379b09f134be29abe6d4535365c827bb4
aanq <Annie163_xaq@yeah.net>
2017-06-02 10:11:22 +0800

    增加 004精品团购列表页，004精品团购详情页，004详情页，004详情页-new2

--- js/main.js
+++ js/main.js
@@ -32,7 +32,9 @@ $(function () {
     })
 })
 $(function () {
+    /*超小屏*/
     if ((screen.width <=767) ) {
+        /*搜索滚动*/
         $(window).scroll(function () {
             var headerTop = $(".navbar-xs").height();
             var searchTop = $("#search-scroll").height();
@@ -42,6 +44,31 @@ $(function () {
                 $("#search-scroll").removeClass("searchBox2-xs-fixed");
             }
         })
+        /*首页 新闻和消息页面高度*/
         $(".containerBox .fixheight").css("height","auto");
+        /*精品列表 申请按钮*/
     }
+    /*sm <800*/
+    if ((screen.width <=991 && screen.width>=768 ) ) {
+        $(".detail-main-box .spcs-hide ").stop().show();
+        $(".detail-main-box .spcs-show ").stop().hide();
+    }
+})
+//返回顶部
+$(function () {
+    $(window).scroll(function () {
+        if($(window).scrollTop() > 100){
+            $(".backTop-box").fadeIn(1000);
+        }else {
+            $(".backTop-box").fadeOut(1000);
+        }
+    });
+    $(".backTop-box").click(function () {
+        var speed = 200;
+        $("body,html").animate({
+                scrollTop:0
+            },
+            speed
+        );
+    })
 })
